2991f2a5f9712e8c1b20cb543e4d7ea16952102d
Satyam Mandal <satyam.mandal.77@gmail.com>
2025-09-05 02:48:34 +0530

    Disable Eslint and TS on Build time

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Skip ESLint
+  },
+  typescript: {
+    ignoreBuildErrors: true, // ✅ Skip TS type errors
+  },
 };
 
 export default nextConfig;
